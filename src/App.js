548c7c845086dca98dba79676fbbc2069ac6a87a
kingslayer845 <ahmed051042@gmail.com>
2022-12-30 22:10:11 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./containers";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/article/Article.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./article.css";
+function article({ imgUrl, date, title }) {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+}
+
+export default article;
